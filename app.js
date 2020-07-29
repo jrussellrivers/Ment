a059887c5174b31dc170dd8f5a3aab34cd2e954a
Carrie Grossman <carriegrossman@MacBook-Pro.local>
2020-07-29 13:41:51 -0400

    made changes to import

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const es6Renderer = require('express-es6-template-engine');
-const connect = require('./config.js')
+const secretInfo = require('./config.js')
 
 const pgp = require('pg-promise')()
 
 const eS = require('express-session')
-const expressSession = eS({secret:'tghvbREGsdgwhwghwrggERgerBHerb', resave: false, saveUninitialized: false})
+const expressSession = eS(secretInfo().secret)
 
 app.use(express.urlencoded({extended: true}))
 app.use(expressSession)
@@ -19,15 +19,9 @@ app.set("view engine", "html")
 
 app.use(express.static("public"));
 
-const connect = {
-    host:'localhost',
-    port:5432,
-    user:'David', //Put your name here for now
-    database:'project_m'
-}
 
 
-const db = pgp(connect)
+const db = pgp(secretInfo().connect)
 require("./api-routes")(app, db);//sets the api
 
 const port = 5434;
