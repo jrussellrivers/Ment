7de5224ada904059ab0e9461e37daa160a82a302
Carrie Grossman <carriegrossman@MacBook-Pro.local>
2020-07-29 13:27:06 -0400

    small updates

--- public/js/createUser.js
+++ public/js/createUser.js
@@ -7,6 +7,7 @@ const createUser = async (req,res,next) => {
     // this needs to be updated 
     let insertion = await db.none(`INSERT INTO users (username, email, password, mentor, about, zipcode) VALUES ($1, $2, $3, $4, $5, $6)`, 
     [req.body.username, req.body.email, hash, req.body.mentorBool, req.body.about, parseInt(req.body.zipcode)])
+    next()
 }
 
 module.exports = createUser
