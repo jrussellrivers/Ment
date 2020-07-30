0c60f69929a6687db93155c0716bd47cabc4c8b1
Jordan Rivers <j.russell.rivers@gmail.com>
2020-07-30 10:35:27 -0400

    chat function added

--- install.sql
+++ install.sql
@@ -13,3 +13,16 @@ CREATE TABLE images (
     user_id INTEGER REFERENCES users (id),
     imgname text
 );
+
+CREATE TABLE chat_rooms (
+    id SERIAL PRIMARY KEY,
+    mentor_id INTEGER REFERENCES users (id),
+    mentee_id INTEGER REFERENCES users (id)
+);
+CREATE TABLE chat_messages (
+    id SERIAL PRIMARY KEY,
+    sent_message VARCHAR,
+    sender VARCHAR,
+    sent_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    chat_id INTEGER REFERENCES chat_rooms (id)
+);
