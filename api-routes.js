a40c8a8a85a7d08c19b6460206ead9cfa6c23f48
David Mitchell <dmitchell217@gmail.com>
2020-07-31 14:05:26 -0400

    updates

--- api-routes.js
+++ api-routes.js
@@ -1,3 +1,6 @@
+//To make your app.js look cleaner use this pattern
+// import checkIsLoggedIn from 'public/js/checkIsLoggedIn.js'
+// const db = require('./app')
 const passport = require('passport')
 const formidable = require("formidable");
 const Strategy = require('passport-local').Strategy
@@ -18,6 +21,12 @@ const bcrypt = require('bcrypt')
 const saltRounds = 10
 
 const apiRoutes = (app, db)=>{
+        //  use static here
+    // app.use(express.static('public')) - create static folders for public, mentor, and mentee
+    // app.get('/', (req, res)=> {
+    //     res.render('./public/index.html')
+    // })
+
     //  this function passed in the database to all routes/middleware
     const passInfo = (req, res, next) => {
         res.db = db
@@ -32,6 +41,7 @@ const apiRoutes = (app, db)=>{
 
     // seperate pg promise
     passport.use(new Strategy((username,password,callback)=>{
+        // console.log(username, password)
         db.one(`SELECT * FROM users WHERE username='${username}'`)
         .then(u=>{
             console.log(u) //
@@ -40,6 +50,7 @@ const apiRoutes = (app, db)=>{
                 if(!result) return callback(null,false)
                 return callback(null, u)
             })
+            // return callback(null, u) // delete/comment this out later
         })
         .catch(()=>callback(null,false))
     }))
@@ -81,61 +92,20 @@ const apiRoutes = (app, db)=>{
                 }
             })
         }
-        const showOr2EeProfile = async () => {
-            res.render("mentorToMentee", {
-                locals: {
-                user: userProfile || {type:"N/A",username:"N/A"},
-                picture: picture,
-                chatlink: '<a href = /chat/' + userProfile.id + '>' + `Chat with ${userProfile.username}` + '</a>'
-                }
-            })
-        }
-        const showEe2OrProfile = async () => {
-            res.render("menteeToMentor", {
-                locals: {
-                user: userProfile || {type:"N/A",username:"N/A"},
-                picture: picture,
-                chatlink: '<a href = /chat/' + userProfile.id + '>' + `Chat with ${userProfile.username}` + '</a>'
-                }
-            })
-        }
-        const showEe2EeProfile = async () => {
-            res.render("mentorToMentee", {
-                locals: {
-                user: userProfile || {type:"N/A",username:"N/A"},
-                picture: picture,
-                chatlink: '<a href = /chat/' + userProfile.id + '>' + `Chat with ${userProfile.username}` + '</a>'
-                }
-            })
-        }
-        const showOr2OrProfile = async () => {
-            res.render("menteeToMentor", {
-                locals: {
-                user: userProfile || {type:"N/A",username:"N/A"},
-                picture: picture,
-                chatlink: '<a href = /chat/' + userProfile.id + '>' + `Chat with ${userProfile.username}` + '</a>'
-                }
-            })
-        }
+
+        // userProfile.type == 'T' ? showMenteeProfile() : showMentorProfile()
         if (userProfile.mentor == false) {
             showMenteeProfile();
-        } else if (userProfile.mentor == true) {
-        showMentorProfile();
-        } else {
-        res.redirect('/');
-        }
+          } else if (userProfile.mentor == true) {
+            showMentorProfile();
+          } else {
+            res.redirect('/');
+          }
     })
     var new_cards = undefined
     app.get(`/lobby`, checkIsLoggedIn, (req,res)=> {
-<<<<<<< HEAD
-<<<<<<< HEAD
         // show boilerplate lobby first which includes a post form for the search.
 
-=======
-        console.log(new_cards)
->>>>>>> origin/master
-=======
->>>>>>> 9cef26e97396c4c6c8e780ae7b7e9fd487460deb
         if (new_cards == undefined){
             new_cards = ''
             res.render("lobby", {
@@ -163,29 +133,27 @@ const apiRoutes = (app, db)=>{
         res.redirect('/lobby')
     })
 
-    
+    // change
     app.get('/login', (req,res)=>res.sendFile(__dirname + '/public/html/login.html'))
 
 
-    
+    // ternary for mentee or mentor, routes to user specific profile
     app.post('/login', passport.authenticate('local'), (req,res)=>{
         res.redirect(`/user/${req.user.id}`)
     })
-    
+    // this is correct.
     app.get('/register', (req,res)=>res.sendFile(__dirname + '/public/html/register.html'))
 
-
+    // this is correct.
     app.post('/register', checkIfExist, createUser, (req,res)=>{
         res.redirect('/login')
     })
-    
+    // look into how to implement this
     app.get('/logout', (req,res)=>{
         req.logout()
         res.redirect('/login')
     })
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     // app.get(`/chat/:id`, async (req, res)=>{
     //     let sender = req.user
     //     let recipient_id = req.params.id
@@ -194,8 +162,6 @@ const apiRoutes = (app, db)=>{
     //     if (room_id == false){res.redirect(`/user/${req.params.id}`)}
     //     else{res.redirect(`/chat/${req.params.id}/${room_id}`)}
     // })
-=======
->>>>>>> 9cef26e97396c4c6c8e780ae7b7e9fd487460deb
 
     app.get('/photos/:id', async (req, res)=> {
         let pic = await getPhoto(req.params.id, db)
@@ -258,38 +224,6 @@ const apiRoutes = (app, db)=>{
             res.json({"url": `/user/${req.user.id}`})
             // res.send(result)
             });
-=======
-    app.post("/image-uploaded", async (req, res) => {
-        let form = {};
-        //this will take all of the fields (including images) and put the value in the form object above
-        new formidable.IncomingForm()
-            .parse(req)
-            .on("field", (name, field) => {
-            form[name] = field;
-            })
-            .on("fileBegin", (name, file) => {
-            //sets the path to save the image
-            file.path =
-                __dirname +
-                "/public/profile_images/" +
-                new Date().getTime() +
-                file.name;
-            })
-            .on("file", (name, file) => {
-            //console.log('Uploaded file', name, file);
-            form.profile_image = file.path.replace(__dirname + "/public", "");
-            })
-
-            .on ("end", async () => {
-            console.log("your photo is uploaded!");
-            
-            //Now i can save the form to the database
-            let newimageaddress= '<img src="' + form.profile_image + '" alt="profile pic">'
-            let results = await db.none("insert into images (user_id, imgname) values ($1, $2)", [req.user.id, newimageaddress])
-            console.log(results)
-            res.json({"url": `/user/${req.user.id}`})
-            });  
->>>>>>> origin/master
     });
     
     app.get('/chat/:id', checkIsLoggedIn, async (req, res)=>{
@@ -300,12 +234,10 @@ const apiRoutes = (app, db)=>{
         else{res.redirect(`/chat/room/${room_id}`)}
     })
     
-    app.get('/chat/room/:id', async (req,res) =>{
-        let return_message = await checkLoggedUser(db, req.user.id, req.params.id)
+    app.get('/chat/room/:id', checkIsLoggedIn, async (req,res) =>{
         let messages = await renderChatRoom(db, req.params.id)
         res.render("chat_room", {
             locals: {
-            return_link:'href="' + return_message + '"',
             room_id:req.params.id,
             messages: messages,
             actionstring:'action="/chat/room/' + req.params.id + '"'
@@ -319,3 +251,8 @@ const apiRoutes = (app, db)=>{
     })
 };
 module.exports = apiRoutes;
+
+//on app.js
+//require("./api-routes")(app); //will load the routes
+
+//you could alternatively use express.router if you want to do the research. should do this
