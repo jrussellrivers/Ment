659f444bf6f6052746c595eb1679daae68de1978
David Mitchell <dmitchell217@gmail.com>
2020-08-03 18:40:09 -0400

    sup

--- public/js/findMentsPic.js
+++ public/js/findMentsPic.js
@@ -28,59 +28,54 @@ const findMents = async (user, category, value, db, url) => {
     //     }
     // }
     // getPhotos(ments)
-
+    let styles = ["grad-pink", "grad-orange", "grad-blue"]
+    let skillString = (notMentor == false ? "My target skills" : "My expertise")
     // then generate hmtl cards for each user result. join at the end. Potentially sort them.
     let new_html = ''
     ments.map((ment, idx)=>{
+        style = styles[idx%3]
+        console.log(style, idx)
         // let pic = pics[idx]
         pic_path = url + '/photos/' + ment.id
         let new_card = 
                 `
                 <form action="/user/${ment.id}" method="get">
-                <button type="submit" class = "cardSubmit">
-                <div class="container">
-        <div class="section">
-            <div class="columns">
-            <div class="column has-text-centered">
-                <h1 class="title" style="color: ghostwhite;">${ment.username}</h1><br>
-            </div>
-            </div>
-            <div id="app" class="row columns is-multiline">
-            <div v-for="card in cardData" key="card.id" class="column is-4">
-                <div class="card large">
-                <div class="card-image is-16by9">
-                    <figure class="image">
-                    <img src=${pic_path} alt="Card Avatar">
-                    </figure>
-                </div>
-                <div class="card-content">
-                    <div class="media">
-                    <div class="media-content">
-                        <p class="title is-4 no-padding">${ment.username}</p>
-                        <p>
-                        <p class="subtitle is-6">
-                        <li><a href="/user/${ment.id}">${ment.username}'s profile</a>
-                        </p>
+                    <button type="submit" class = "cardSubmit btn-atom">
+                        <div class="container">
+                             <div class="section-content">
+                                <div class="columns">
+                                <div class="column has-text-centered">
+                                    <h1 class="title" style="color: ghostwhite;">${ment.username}</h1><br>
+                                </div>
+                                </div>
+                                <div id="app" class="row columns is-multiline">
+                                    <div v-for="card in cardData" key="card.id" class="column is-4">
+                                        <div class="card large">
+                                            <div class="card-image is-sqaure">
+                                                <figure class="image">
+                                                    <img src=${pic_path} alt="Card Avatar">
+                                                </figure>
+                                            </div>
+                                            <div class="card-content ${style}">
+                                                <div class="media">
+                                                    <div class="media-content">
+                                                        <p class="title is-4 no-padding">${ment.username}</p>
+                                                        <p>
+                                                            <p class="subtitle is-6"> <a href="/user/${ment.id}">${ment.username}'s profile</a>
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                                <div class="content">${ment.about}</div>
+                                                <div class="content">Contact me at ${ment.email}</div>
+                                                <div class="content">${skillString}: ${skillSets[idx]}</div>
+                                        </div>  
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    </div>
-                    <div class="content">
-                    ${ment.about}
-                    </div>
-                    <div class="content">
-                    Contact me at ${ment.email}
-                    </div>
-                    <div class="content">
-                    My Expertise: ${skillSets[idx]}
-                    </div>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div>
-        </div>
-        </button>
-        </form>
-
+                </button>
+            </form>
                 `
     new_html = new_html + new_card
     }).join("")
