7ea817d093bc76356319041d045cc0dd0d67f941
pfarrell18 <66265731+pfarrell18@users.noreply.github.com>
2020-08-03 09:05:46 -0400

    chat

--- public/js/renderChatRoom.js
+++ public/js/renderChatRoom.js
@@ -1,15 +1,25 @@
 const renderChatRoom = async (db, room_id) =>{
     let messages = await db.any(`SELECT * FROM chat_messages WHERE chat_id='${room_id}'`)
+    let room = await db.one(`SELECT * FROM chat_rooms WHERE id='${room_id}'`)
+    let mentee = await db.one(`SELECT * FROM users WHERE id='${room.mentee_id}'`)
+ 
     new_html = ''
+
+    
     messages.map(message=>{
         let date = message.sent_at
         let new_date = date.toGMTString()
-        let new_message = `<div class='message'>
-        <span class='message-name'>${message.sender}  </span>
-        <span class='message-time'>[${new_date}]  </span>:
-        <span class='message-message'>  ${message.sent_message}</span>
-        <div>`
-        new_html = new_html + new_message
+        if (message.sender == mentee.username){
+            let new_message = `<tr><td class ='has-text-primary-dark'><strong>${message.sender}</strong><br>${new_date}</td>
+            <td class='message-message'>  ${message.sent_message}</td></tr>
+            `
+            new_html = new_html + new_message
+        }else{
+            let new_message = `<tr><td class ='has-text-primary-dark'><strong>${message.sender}</strong><br>${new_date}</td>
+            <td class='message-message'>  ${message.sent_message}</td></tr>
+            `
+            new_html = new_html + new_message
+        }
     }).sort((a,b)=>a.sent_at-b.sent_at).join("")
     return new_html
 }
